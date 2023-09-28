28ab18778a03b674a30d1040f8cf02d6b39bea89
Oscar Lemstra <61353780+oscarlemstra@users.noreply.github.com>
2023-09-28 17:43:02 +0200

    made a new sudoku fill method
    fills block 1

---
+++ sudoku_fillers/fillSudokuMethod03.js
@@ -0,0 +1,18 @@
+// fill sudoku puzzle method 3
+let numbers = [1,2,3,4,5,6,7,8,9];
+let ignoreNumbers = [];
+
+
+// fills block 1
+for (mainY = 0; mainY < 3; mainY++) {
+    for (mainX = 0; mainX < 3; mainX++) {
+        setIgnoreNumbers(0, 3, 0, 3);
+        setIgnoreNumbers(mainY, mainY+1, 0, 9);
+        setIgnoreNumbers(0, 9, mainX, mainX+1);
+        removeElementsToIgnoreFromNumbers();
+        setSudokuValuesOfGivenPos(mainY, mainY+1, mainX, mainX+1);
+
+        numbers = [1,2,3,4,5,6,7,8,9];
+        ignoreNumbers = [];
+    }
+}
