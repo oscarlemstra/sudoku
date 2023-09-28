a693cb829716af1eee25c4444cd5f83ce7222352
Oscar Lemstra <61353780+oscarlemstra@users.noreply.github.com>
2023-09-28 20:47:52 +0200

    fills block 4, 5 and 6

--- sudoku_fillers/fillSudokuMethod03.js
+++ sudoku_fillers/fillSudokuMethod03.js
@@ -1,6 +1,8 @@
 // fill sudoku puzzle method 3
 let numbers = [1,2,3,4,5,6,7,8,9];
 let ignoreNumbers = [];
+let canGenerate = true;
+let generateCount = 0;
 
 
 // fills block 1
@@ -46,4 +48,59 @@ for (mainY = 0; mainY < 3; mainY++) {
         numbers = [1,2,3,4,5,6,7,8,9];
         ignoreNumbers = [];
     }
+}
+
+// fills block 4
+while (checkSudokuBlockForUndefined(3, 6, 0, 3)) {
+    for (mainY = 3; mainY < 6; mainY++) {
+        for (mainX = 0; mainX < 3; mainX++) {
+            setIgnoreNumbers(3, 6, 0, 3);
+            setIgnoreNumbers(mainY, mainY+1, 0, 9);
+            setIgnoreNumbers(0, 9, mainX, mainX+1);
+            removeElementsToIgnoreFromNumbers();
+            setSudokuValuesOfGivenPos(mainY, mainY+1, mainX, mainX+1);
+
+            numbers = [1,2,3,4,5,6,7,8,9];
+            ignoreNumbers = [];
+        }
+    }
+    console.log("block 4");
+}
+
+// fills block 5
+while (checkSudokuBlockForUndefined(3, 6, 3, 6)) {
+    for (mainY = 3; mainY < 6; mainY++) {
+        for (mainX = 3; mainX < 6; mainX++) {
+            setIgnoreNumbers(3, 6, 3, 6);
+            setIgnoreNumbers(mainY, mainY+1, 0, 9);
+            setIgnoreNumbers(0, 9, mainX, mainX+1);
+            removeElementsToIgnoreFromNumbers();
+            setSudokuValuesOfGivenPos(mainY, mainY+1, mainX, mainX+1);
+
+            numbers = [1,2,3,4,5,6,7,8,9];
+            ignoreNumbers = [];
+        }
+    }
+    console.log("block 5");
+}
+
+// fills block 6
+while (checkSudokuBlockForUndefined(3, 6, 6, 9) && canGenerate) {
+    for (mainY = 3; mainY < 6; mainY++) {
+        for (mainX = 6; mainX < 9; mainX++) {
+            setIgnoreNumbers(3, 6, 6, 9);
+            setIgnoreNumbers(mainY, mainY+1, 0, 9);
+            setIgnoreNumbers(0, 9, mainX, mainX+1);
+            removeElementsToIgnoreFromNumbers();
+            setSudokuValuesOfGivenPos(mainY, mainY+1, mainX, mainX+1);
+
+            numbers = [1,2,3,4,5,6,7,8,9];
+            ignoreNumbers = [];
+        }
+    }
+    generateCount++;
+
+    if (generateCount > 50) { canGenerate = false; }
+
+    console.log("block 6");
 }
