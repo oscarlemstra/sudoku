de1a08b33991058eced82ed445c0054a92bc7dc9
Oscar Lemstra <61353780+oscarlemstra@users.noreply.github.com>
2023-09-28 22:24:37 +0200

    fills block 8

--- sudoku_fillers/fillSudokuMethod03.js
+++ sudoku_fillers/fillSudokuMethod03.js
@@ -131,4 +131,30 @@ while (canGenerateSudoku) {
             }
         }
     }
+
+    // fills block 8
+    while (checkSudokuBlockForUndefined(6, 9, 3, 6) && canGenerateBlock) {
+        for (mainY = 6; mainY < 9; mainY++) {
+            for (mainX = 3; mainX < 6; mainX++) {
+                setIgnoreNumbers(6, 9, 3, 6);
+                setIgnoreNumbers(mainY, mainY+1, 0, 9);
+                setIgnoreNumbers(0, 9, mainX, mainX+1);
+                removeElementsToIgnoreFromNumbers();
+                setSudokuValuesOfGivenPos(mainY, mainY+1, mainX, mainX+1);
+
+                numbers = [1,2,3,4,5,6,7,8,9];
+                ignoreNumbers = [];
+            }
+        }
+        generateCount++;
+
+        if (generateCount > 50) {
+            canGenerateSudoku = true;
+            canGenerateBlock = false;
+            emptyGivenSudokuPos(0, 9, 0, 9);
+        }
+
+        console.log("block 8");
+    }
+    generateCount = 0;
 }
