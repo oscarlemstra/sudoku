0e428f364148b094c1eecec9dae07429bf6cea5d
Oscar Lemstra <61353780+oscarlemstra@users.noreply.github.com>
2024-01-15 16:55:45 +0100

    added a debug log for in the browser console
    It shows the amount of removed sudoku elements of the 81 elements

--- makeSudokuPuzzle.js
+++ makeSudokuPuzzle.js
@@ -4,6 +4,7 @@ for (y = 0; y < 9; y++) {
     for (x = 0; x < 9; x++) {
         if (randomInt(101, 0) > 90) {
             sudokuElementsArray[y][x].innerText = "";
+            console.log("empty sudoku element")
         }
     }
 }
