11b9f68d6f9465528a685c5172c5a99402c6b691
Oscar Lemstra <61353780+oscarlemstra@users.noreply.github.com>
2024-02-16 14:26:10 +0100

    reworked the navigation of the site

--- sudokuDifficultySelect.js
+++ sudokuDifficultySelect.js
@@ -10,31 +10,31 @@ let level_5 = document.getElementById("level-5");
 // sudokuElementsRemovePercentage:   80, 70, 60, 50, 40
 
 level_1.addEventListener("click", function () {
-    window.location.href = "./index.html";
+    window.location.href = "./puzzle.html";
     sessionStorage.setItem("sudokuElementsToRemoveAmount", "24");
     sessionStorage.setItem("sudokuElementsRemovePercentage", "80");
 });
 
 level_2.addEventListener("click", function () {
-    window.location.href = "./index.html";
+    window.location.href = "./puzzle.html";
     sessionStorage.setItem("sudokuElementsToRemoveAmount", "34");
     sessionStorage.setItem("sudokuElementsRemovePercentage", "70");
 });
 
 level_3.addEventListener("click", function () {
-    window.location.href = "./index.html";
+    window.location.href = "./puzzle.html";
     sessionStorage.setItem("sudokuElementsToRemoveAmount", "44");
     sessionStorage.setItem("sudokuElementsRemovePercentage", "60");
 });
 
 level_4.addEventListener("click", function () {
-    window.location.href = "./index.html";
+    window.location.href = "./puzzle.html";
     sessionStorage.setItem("sudokuElementsToRemoveAmount", "54");
     sessionStorage.setItem("sudokuElementsRemovePercentage", "50");
 });
 
 level_5.addEventListener("click", function () {
-    window.location.href = "./index.html";
+    window.location.href = "./puzzle.html";
     sessionStorage.setItem("sudokuElementsToRemoveAmount", "64");
     sessionStorage.setItem("sudokuElementsRemovePercentage", "40");
 });
