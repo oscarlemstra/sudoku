d0e033439adf3a7f74df1dbd76aa415d7bee9942
Oscar Lemstra <61353780+oscarlemstra@users.noreply.github.com>
2024-02-19 21:50:25 +0100

    made the function setNote

--- notesFunctionality.js
+++ notesFunctionality.js
@@ -23,7 +23,6 @@ function addNotesElement (element) {
     for (i = 0; i < 9; i++) {
         let childNote = document.createElement("div");
         childNote.classList.add("note");
-        childNote.innerText = i; // if code is done remove this line
 
         parentDivForNotes.appendChild(childNote);
     }
@@ -43,5 +42,48 @@ function doesNotesElementExist (element) {
     }
     else {
         return false;
+    }
+}
+
+function setNote (element) {
+    let notes = element.firstChild.children;
+
+    function switchNoteValue (note, stringNumber) {
+        if (note.innerText === "") {
+            note.innerText = stringNumber;
+        }
+        else {
+            note.innerText = "";
+        }
+    }
+
+    switch (selectedNumber) {
+        case "1":
+            switchNoteValue(notes[0], "1");
+            break;
+        case "2":
+            switchNoteValue(notes[1], "2");
+            break;
+        case "3":
+            switchNoteValue(notes[2], "3");
+            break;
+        case "4":
+            switchNoteValue(notes[3], "4");
+            break;
+        case "5":
+            switchNoteValue(notes[4], "5");
+            break;
+        case "6":
+            switchNoteValue(notes[5], "6");
+            break;
+        case "7":
+            switchNoteValue(notes[6], "7");
+            break;
+        case "8":
+            switchNoteValue(notes[7], "8");
+            break;
+        case "9":
+            switchNoteValue(notes[8], "9");
+            break;
     }
 }
