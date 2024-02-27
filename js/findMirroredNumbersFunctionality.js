e91218180ccae704d86de0393a6abd7d685ac198
Oscar Lemstra <61353780+oscarlemstra@users.noreply.github.com>
2024-02-27 22:25:32 +0100

    fixed the code
    the function didn't work before, now it does

--- js/findMirroredNumbersFunctionality.js
+++ js/findMirroredNumbersFunctionality.js
@@ -4,21 +4,21 @@ function findMirroredNumbers () {
     }
 
     function debugArrayElement (firstIndex, secondIndex) {
-        sudokuElementsArray[firstIndex][secondIndex].style.color = "orange";
+        sudokuElementsArray[firstIndex][secondIndex].style.color = "white";
     }
 
     // parameter info
     // n = number
     // p = position
     // y and x stand for the axis of the array
-    function searchForMirrored (n1yp,n1xp, n2yp,n2xp, n3yp,n3xp, n4yp,n4xp, searchLength) {
-        for (mainOffset = 0; mainOffset < 3; mainOffset++) {
-            for (secondOffset = 0; secondOffset < searchLength; secondOffset++) {
-                if (getArrayElement(n1yp,n1xp+mainOffset) === getArrayElement(n2yp,n2xp+secondOffset) && getArrayElement(n3yp,n3xp+mainOffset) === getArrayElement(n4yp,n4xp+secondOffset)) {
-                    debugArrayElement(n1yp,n1xp+mainOffset);
-                    debugArrayElement(n2yp,n2xp+secondOffset);
-                    debugArrayElement(n3yp,n3xp+mainOffset);
-                    debugArrayElement(n4yp,n4xp+secondOffset);
+    function searchForMirrored (n1yp,n1xp, n2yp,n2xp, n3yp,n3xp, n4yp,n4xp, searchLengthY, searchLengthX) {
+        for (OffsetY = 0; OffsetY < searchLengthY; OffsetY++) {
+            for (OffsetX = 0; OffsetX < searchLengthX; OffsetX++) {
+                if (getArrayElement(n1yp,n1xp) === getArrayElement(n2yp+OffsetY,n2xp+OffsetX) && getArrayElement(n3yp+OffsetY,n3xp) === getArrayElement(n4yp,n4xp+OffsetX)) {
+                    debugArrayElement(n1yp,n1xp);
+                    debugArrayElement(n2yp+OffsetY,n2xp+OffsetX);
+                    debugArrayElement(n3yp+OffsetY,n3xp);
+                    debugArrayElement(n4yp,n4xp+OffsetX);
                 }
                 else {
                     console.log("no mirrored numbers found");
@@ -27,5 +27,5 @@ function findMirroredNumbers () {
         }
     }
 
-    searchForMirrored(0,0, 1,3, 1,0, 0,3, 6);
+    searchForMirrored(0,0, 1,1, 1,0, 0,1, 8, 8);
 }
