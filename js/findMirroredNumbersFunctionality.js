f8ba62b9a4a72e714cadb63a4e4c537349bb01d3
Oscar Lemstra <61353780+oscarlemstra@users.noreply.github.com>
2024-02-28 17:12:18 +0100

    added all search functions for block 2

--- js/findMirroredNumbersFunctionality.js
+++ js/findMirroredNumbersFunctionality.js
@@ -41,4 +41,20 @@ function findMirroredNumbers () {
     // searchForMirrored(2,0, 3,1, 3,0, 2,1, 6, 8);
     // searchForMirrored(2,1, 3,2, 3,1, 2,2, 6, 7);
     // searchForMirrored(2,2, 3,3, 3,2, 2,3, 6, 6);
+
+
+    // Block 2 - Row 1
+    // searchForMirrored(0,3, 1,4, 1,3, 0,4, 8, 5);
+    // searchForMirrored(0,4, 1,5, 1,4, 0,5, 8, 4);
+    // searchForMirrored(0,5, 1,6, 1,5, 0,6, 8, 3);
+
+    // Block 2 - Row 2
+    // searchForMirrored(1,3, 2,4, 2,3, 1,4, 7, 5);
+    // searchForMirrored(1,4, 2,5, 2,4, 1,5, 7, 4);
+    // searchForMirrored(1,5, 2,6, 2,5, 1,6, 7, 3);
+
+    // Block 2 - Row 3
+    // searchForMirrored(2,3, 3,4, 3,3, 2,4, 6, 5);
+    // searchForMirrored(2,4, 3,5, 3,4, 2,5, 6, 4);
+    // searchForMirrored(2,5, 3,6, 3,5, 2,6, 6, 3);
 }
